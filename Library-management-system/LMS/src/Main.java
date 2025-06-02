803cb79cb5b8b9cbc82c5d0fbd0b465af965d639
MohaamedHamdy <116814421+MohaamedHamdy@users.noreply.github.com>
2025-06-03 00:36:31 +0300

    update main

--- Library-management-system/LMS/src/Main.java
+++ Library-management-system/LMS/src/Main.java
@@ -1,40 +1,44 @@
-import Models.Admin;
-import Models.Student;
 import Models.User;
+import Service.Authentication;
+import Service.Database;
 
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-//        User admin = new Admin("Mohamd", "mhmd@gmail.com", "123456");
-//        System.out.println(admin);
-//        User admin2 = new Admin("Mohamd", "mhmd@gmail.com", "123456");
-//        System.out.println(admin2);
-//        User admin3 = new Admin("Mohamd", "mhmd@gmail.com", "123456");
-//        System.out.println(admin3);
-//        User student = new Student("Mohamd", "mhmd@gmail.com", "123456");
-//        System.out.println(student);
-//        User student2 = new Student("Mohamd", "mhmd@gmail.com", "123456");
-//        System.out.println(student);
-        System.out.println("Welcome to LMS");
-        int choice;
-        do {
-            System.out.println("--Main menu--");
-            System.out.println("1-Login\n2-Signup\n");
-            try {
-                Scanner in = new Scanner(System.in);
-                choice = in.nextInt();
-                break;
-            } catch (Exception e) {
-                System.out.println("u must enter number");
-            }
-        }while (true);
 
-        switch (choice){
-            case 1 -> System.out.println("login");
-            case 2 -> System.out.println("Signup");
-            default -> System.out.println("Error");
+    public static void main(String[] args) {
+        Database database = new Database();
+        Authentication authentication = new Authentication();
+        User user = null;
+
+        while (true){
+            System.out.println("Welcome to LMS");
+            int choice = -1;
+            System.out.println("--Main menu--");
+            System.out.println("1-Login\n2-Signup\n3-Exit");
+            choice = getChoice(choice);
+            switch (choice){
+                case 1 -> System.out.println("login");
+                case 2 -> user = authentication.signUp("mohamed", "mhmd@gmail.com", "123");
+                case 3 -> {
+                    break;
+                }
+                default -> System.out.println("Error");
+            }
         }
 
+    }
+
+    public static int getChoice(int choice){
+        while (true){
+            Scanner in = new Scanner(System.in);
+            try {
+                choice = in.nextInt();
+                return choice;
+            } catch (Exception e) {
+                System.out.println("Input must be number");
+                in.nextLine();
+            }
+        }
     }
 }
