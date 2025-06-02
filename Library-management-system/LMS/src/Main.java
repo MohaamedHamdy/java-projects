9afdbba13bb49cf859ef75426556010b79ad86f3
MohaamedHamdy <116814421+MohaamedHamdy@users.noreply.github.com>
2025-06-03 00:04:06 +0300

    basic main created

--- Library-management-system/LMS/src/Main.java
+++ Library-management-system/LMS/src/Main.java
@@ -1,5 +1,40 @@
+import Models.Admin;
+import Models.Student;
+import Models.User;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+//        User admin = new Admin("Mohamd", "mhmd@gmail.com", "123456");
+//        System.out.println(admin);
+//        User admin2 = new Admin("Mohamd", "mhmd@gmail.com", "123456");
+//        System.out.println(admin2);
+//        User admin3 = new Admin("Mohamd", "mhmd@gmail.com", "123456");
+//        System.out.println(admin3);
+//        User student = new Student("Mohamd", "mhmd@gmail.com", "123456");
+//        System.out.println(student);
+//        User student2 = new Student("Mohamd", "mhmd@gmail.com", "123456");
+//        System.out.println(student);
+        System.out.println("Welcome to LMS");
+        int choice;
+        do {
+            System.out.println("--Main menu--");
+            System.out.println("1-Login\n2-Signup\n");
+            try {
+                Scanner in = new Scanner(System.in);
+                choice = in.nextInt();
+                break;
+            } catch (Exception e) {
+                System.out.println("u must enter number");
+            }
+        }while (true);
+
+        switch (choice){
+            case 1 -> System.out.println("login");
+            case 2 -> System.out.println("Signup");
+            default -> System.out.println("Error");
+        }
+
     }
 }
