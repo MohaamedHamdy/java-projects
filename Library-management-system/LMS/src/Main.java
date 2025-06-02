c456089cc7d009235f8dcc97e606df535cfe0e3a
MohaamedHamdy <116814421+MohaamedHamdy@users.noreply.github.com>
2025-06-03 00:56:53 +0300

    create authentication view class and sign up function

--- Library-management-system/LMS/src/Main.java
+++ Library-management-system/LMS/src/Main.java
@@ -1,37 +1,49 @@
 import Models.User;
 import Service.Authentication;
 import Service.Database;
+import Views.AuthenticationView;
 
 import java.util.Scanner;
 
 public class Main {
+    private static Scanner in = new Scanner(System.in);
 
     public static void main(String[] args) {
-        Database database = new Database();
-        Authentication authentication = new Authentication();
-        User user = null;
+        System.out.println("Welcome to LMS");
 
-        while (true){
-            System.out.println("Welcome to LMS");
+        boolean isRun = true;
+
+        Database database = new Database();
+        Authentication authentication = new Authentication(database);
+        User user = null;
+        AuthenticationView authenticationView = new AuthenticationView(authentication);
+
+
+        while (isRun) {
             int choice = -1;
-            System.out.println("--Main menu--");
+            System.out.println("\n--Main menu--");
             System.out.println("1-Login\n2-Signup\n3-Exit");
             choice = getChoice(choice);
-            switch (choice){
-                case 1 -> System.out.println("login");
-                case 2 -> user = authentication.signUp("mohamed", "mhmd@gmail.com", "123");
-                case 3 -> {
+            switch (choice) {
+                case 1:
+                    System.out.println("login");
                     break;
-                }
-                default -> System.out.println("Error");
+                case 2:
+                    user = authenticationView.signUp();
+                    System.out.println(user);
+                    break;
+                case 3:
+                    isRun = false;
+                    break;
+                default:
+                    System.out.println("Error");
             }
         }
 
     }
 
-    public static int getChoice(int choice){
-        while (true){
-            Scanner in = new Scanner(System.in);
+    public static int getChoice(int choice) {
+        while (true) {
             try {
                 choice = in.nextInt();
                 return choice;
